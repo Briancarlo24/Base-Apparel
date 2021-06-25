a5d7424a5ff81fa2cae34ada84ad0722104f161a
Briancarlo24 <Briancarlo2494@gmail.com>
2021-06-25 15:02:13 +0800

    Finally Completed. Only need to add the responsiveness

--- css/script.js
+++ css/script.js
@@ -1,3 +1,27 @@
-document.querySelectorAll("#email").addEventListener("onFocus", function() {
-  
-});
+document.querySelectorAll(".btn-email")[0].addEventListener("click", validate);
+
+// Get the borderColor of the input
+let borderColor = document.getElementById("mail");
+let myBorderColor = window.getComputedStyle(borderColor).borderColor;
+
+function validate() {
+  let email = document.getElementById("mail").value;
+  if (validateEmail(email)) {
+    document.getElementById("mail").style.border = "1px solid";
+    document.getElementById("mail").style.borderColor = "green";
+    alert(email + " is Valid" + " the color is: " + myBorderColor)
+    return true;
+  };
+  document.getElementById("mail").style.border = "1px solid";
+  document.getElementById("mail").style.borderColor = "#f96262";
+  document.querySelector(".checker").style.visibility = "visible";
+  document.querySelector(".icon").style.visibility = "visible";
+  alert(email + " is Invalid")
+  return false;
+};
+
+// To Check the validiy of the email Address
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
