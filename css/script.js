cf210eef3a11f7603816170ddf4079b47e9ddea1
Briancarlo24 <Briancarlo2494@gmail.com>
2021-06-28 14:31:00 +0800

    update Javascript

--- css/script.js
+++ css/script.js
@@ -9,6 +9,8 @@ function validate() {
   if (validateEmail(email)) {
     document.getElementById("mail").style.border = "1px solid";
     document.getElementById("mail").style.borderColor = "#f96262";
+    alert("email " + email + " is valid");
+    refreshPage();
     return true;
   };
   document.getElementById("mail").style.border = "1px solid";
@@ -22,4 +24,8 @@ function validate() {
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
+}
+
+function refreshPage(){
+    window.location.reload();
 }
